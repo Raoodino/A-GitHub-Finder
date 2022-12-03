b7184ee4c5b490939fbefd50c4e21d7b1622f5e8
jeriraooo <jerirao@foxmail.com>
2022-12-03 19:56:53 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  //all contents contain daisyUI to be put here
   content: [
     "./src/App.js",
     "./src/components/layout/Navbar.jsx"
